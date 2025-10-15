2cbadfa0343143392b423bbd2e5d4c6f0cece015
Mario German <arancibigm@gmail.com>
2025-10-15 19:38:22 +0200

    update folders

--- 352/Cordinates.py
+++
@@ -1,9 +0,0 @@
-class Cordenadas:
-    def __init__(self,px="",py=""):
-        self.__x = px 
-        self.__y = py
-    def getter(self):
-        return f"x:{self.__x} y:{self.__y}\n"
-    def setter(self,px="",py=""):
-        self.__x = px 
-        self.__y = py
--- 353/Figure.py
+++
@@ -1,5 +0,0 @@
-class Figura:
-    def __init__(self, pcordenadas):
-        self.__cordenadas = pcordenadas
-    def getcordenadas(self):
-        return self.__cordenadas
